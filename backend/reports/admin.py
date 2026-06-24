3681911ebbd5a05a4e4922cb97eedc45c8be55a1
Shivam Gami <shivamprakashgami@gmail.com>
2026-06-24 23:38:54 +0530

    Delete backend directory

--- backend/reports/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import MedicalReport
-
-@admin.register(MedicalReport)
-class MedicalReportAdmin(admin.ModelAdmin):
-    list_display = ("original_filename", "file_size_kb", "uploaded_at")
--- backend/reports/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import UploadReportView, DownloadReportPDF, ReportHistoryView, DashboardView
-
-urlpatterns = [
-    path("upload/", UploadReportView.as_view()),
-    path("download/<int:report_id>/", DownloadReportPDF.as_view()),
-    path("history/", ReportHistoryView.as_view()),
-    path("dashboard/", DashboardView.as_view()),
-]
